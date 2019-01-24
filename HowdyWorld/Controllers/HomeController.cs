0230378c7669c8a315282c970f4ed8cdba50eda9
NoahFlowa <osterhoutn@gmail.com>
2019-01-24 10:18:02 -0500

    Initial Commit

---
+++ HowdyWorld/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HowdyWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public string String()
+        {
+            return "Howdy, World!";
+        }
+
+        public string Param(int id)
+        {
+            return "ID:" + id;
+        }
+
+        public ActionResult Redirect()
+        {
+            return View("About");
+        }
+
+    }
+}
